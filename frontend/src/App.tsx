514ca6283c9e075f275754b80e7710cc21a1c817
jonassbarai <jonas_sbarai@hotmail.com>
2022-07-14 20:36:02 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton";
 
 function App() {
  return(
-   <h1>hello world</h1>
+   <>
+   <h1>hello world</h1> 
+   <NotificationButton/>
+   <NotificationButton/>
+   </>
  ) 
 }
 
